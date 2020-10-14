89f1416840dae5e1e07310ca41f4c023d89df5ea
Junior <36310159+souzajunior@users.noreply.github.com>
2020-10-14 11:12:52 -0300

    Adjusted comparation

--- Program to find smallest element in array.py
+++ Program to find smallest element in array.py
@@ -2,7 +2,7 @@ arrayIN = [int(i) for i in input("Enter the array (separated by comma ','): ").s
 
 smallest = arrayIN[0]
 for i in range(1, len(arrayIN)):
-        if (arrayIN[i] <= smallest):
+        if (arrayIN[i] < smallest):
                 smallest = arrayIN[i]
 
 print("The smallest element in array is:", smallest)
