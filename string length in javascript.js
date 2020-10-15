4628dd2318f176254c21391fb1fb3bb89bca20ab
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2020-10-16 00:26:25 +0530

    string length in javascript

---
+++ string length in javascript.js
@@ -0,0 +1,12 @@
+<html>
+   <head>
+      <title>Program to find string length</title>
+   </head>
+   
+   <body>   
+      <script type = "text/javascript">
+         var str = new String( "This is string" );
+         document.write("str.length is:" + str.length); 
+      </script>      
+   </body>
+</html>
