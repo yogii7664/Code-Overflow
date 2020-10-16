7330bfb0f17db8eda688aea132c7305d70ffc199
anmolrk <60808502+anmolrk@users.noreply.github.com>
2020-10-16 17:28:59 +0530

    Pre-define ProgramToConvertBinaryToDecimalNumber
    Using Pre-define Function Program To Convert Binary To Decimal Number
    with C, C++, JAVA, Python

---
+++ Program to convert binary to decimal number Pre-define Function/ProgramToConvertBinaryToDecimalNumber.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	char binaryNumber[] = "1001";
+	int bin, dec = 0;
+	
+	bin = atoi(binaryNumber);
+
+	for(int i = 0; bin; i++, bin /= 10) 
+		if (bin % 10)
+			dec += pow(2, i);
+
+	printf("%d", dec);
+
+	return 0;
+}
---
+++ Program to convert binary to decimal number Pre-define Function/ProgramToConvertBinaryToDecimalNumber.java
@@ -0,0 +1,7 @@
+class ConBintoDEC {
+	public static void main(String args[])
+	{
+		String binaryNumber = "1001";
+		System.out.println(Integer.parseInt(binaryNumber, 2));
+	}
+}
